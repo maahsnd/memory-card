fdbbe68a72f0e79a95d398269c36f1b70e7ee906
mrwindupbirdesq <nmaahs11@gmail.com>
2023-05-11 14:32:05 -0400

    Updates

---
+++ precache-manifest.6fb1c4c5f9adab9e70e606b99e825d06.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0307da72afd97cdfdd2",
+    "url": "/mrwindupbirdesq/memory-card/static/css/main.4ebc6e0c.chunk.css"
+  },
+  {
+    "revision": "e0307da72afd97cdfdd2",
+    "url": "/mrwindupbirdesq/memory-card/static/js/main.e0307da7.chunk.js"
+  },
+  {
+    "revision": "c199c0ff3120a1f0a665",
+    "url": "/mrwindupbirdesq/memory-card/static/js/1.c199c0ff.chunk.js"
+  },
+  {
+    "revision": "3324948cdbb1b67ba2c0",
+    "url": "/mrwindupbirdesq/memory-card/static/js/runtime~main.3324948c.js"
+  },
+  {
+    "revision": "f75235539f1a766d93c683c47483e33a",
+    "url": "/mrwindupbirdesq/memory-card/static/media/aang.f7523553.jpeg"
+  },
+  {
+    "revision": "be6ad1a152a053d336c44ff72ee0169e",
+    "url": "/mrwindupbirdesq/memory-card/static/media/appa.be6ad1a1.jpeg"
+  },
+  {
+    "revision": "5cfbbece6be0fd39fc2d9573e432ad92",
+    "url": "/mrwindupbirdesq/memory-card/static/media/iroh.5cfbbece.jpeg"
+  },
+  {
+    "revision": "b963c1f94c6040b0b4b8a8ecd58ab79d",
+    "url": "/mrwindupbirdesq/memory-card/static/media/katara.b963c1f9.jpeg"
+  },
+  {
+    "revision": "fceef694a22e3bb338183d4a200f2963",
+    "url": "/mrwindupbirdesq/memory-card/static/media/momo.fceef694.jpeg"
+  },
+  {
+    "revision": "1ce8cdfee14551c4eeb022da114e4690",
+    "url": "/mrwindupbirdesq/memory-card/static/media/sokka.1ce8cdfe.jpeg"
+  },
+  {
+    "revision": "690564430b9e2f759caeb31e827cad8e",
+    "url": "/mrwindupbirdesq/memory-card/static/media/zuko.69056443.jpeg"
+  },
+  {
+    "revision": "151382a8ad551af64708cf7fb3f418d8",
+    "url": "/mrwindupbirdesq/memory-card/index.html"
+  }
+];
